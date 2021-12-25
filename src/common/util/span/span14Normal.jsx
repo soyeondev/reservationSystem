b7647e4061bf0a0995062653a28b9f85d850b6ed
김소연 <soyeon.dev@gmail.com>
2021-12-25 21:21:45 +0900

    span 컴포넌트 추가

---
+++ src/common/util/span/span14Normal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./span.module.css";
+
+const Span14Normal = (props) => {
+  return (
+    <>
+      <span className={styles.span__14__normal}>{props.text}</span>
+    </>
+  );
+};
+
+export default Span14Normal;
