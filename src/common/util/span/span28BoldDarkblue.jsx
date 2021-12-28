01733151979438835d15ca7853cd1919070aa3bc
김소연 <soyeon.dev@gmail.com>
2021-12-28 23:45:56 +0900

    span28BoldDarblud 컴포넌트 추가

---
+++ src/common/util/span/span28BoldDarkblue.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./span.module.css";
+
+const Span28BoldDarkblue = (props) => {
+  return (
+    <>
+      <span className={styles.span__28__bold__darkblue__area}>
+        {props.text}
+      </span>
+    </>
+  );
+};
+
+export default Span28BoldDarkblue;
