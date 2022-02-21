9a1a9bf95c78c69dd9ab80180c95d3f520f6444d
김소연 <soyeon.dev@gmail.com>
2022-02-21 23:10:22 +0900

    history element css 업데이트

--- src/pages/reservation_history/component/historyElement.jsx
+++ src/pages/reservation_history/component/historyElement.jsx
@@ -6,7 +6,7 @@ import { useHistory } from "react-router-dom";
 import RESERVATION_TYPE from "../data/reservationType";
 
 const HistoryElement = (props) => {
-  const MedicalHistoryElement = (props) => {
+  const HistoryElement = (props) => {
   const {
     date,
     time,
@@ -93,29 +93,29 @@ const HistoryElement = (props) => {
   
   return (
     <article
-      className={styles.medical__history__element__area}
+      className={styles.history__element__area}
       onClick={onClick}
     >
-      <section className={styles.medical__history__info__area}>
+      <section className={styles.history__info__area}>
         <div>
           <img
             src={medicalHistory}
             className={styles.calendar__img}
             alt="medical_history"
           />
-          <span className={styles.medical__history__date}>{date}</span>
-          <span className={styles.medical__history__date}>{time}</span>
+          <span className={styles.history__date}>{date}</span>
+          <span className={styles.history__date}>{time}</span>
         </div>
-        <div className={styles.medical__history__doc__area}>
+        <div className={styles.history__doc__area}>
           <div>
-            <span className={styles.medical__history__doctor}>
+            <span className={styles.history__doctor}>
               {doctorName}
             </span>
-            <span className={styles.medical__history__hospital}>
+            <span className={styles.history__hospital}>
               {hospitalName}
             </span>
           </div>
-          <div className={styles.medical__history__type}>
+          <div className={styles.history__type}>
             비대면진료({kind === 3 ? "전화진료" : "화상진료"})
           </div>
         </div>
@@ -124,14 +124,14 @@ const HistoryElement = (props) => {
         <button
           className={
             type === 1
-              ? styles.medical__history__button__waiting
+              ? styles.history__button__waiting
               : type === 2
-              ? styles.medical__history__button__complate
+              ? styles.history__button__complate
               : type === 4
-              ? styles.medical__history__button__dignosis__complate
+              ? styles.history__button__dignosis__complate
               : type === 9
-              ? styles.medical__history__button__cancle
-              : styles.medical__history__button__complate
+              ? styles.history__button__cancle
+              : styles.history__button__complate
           }
         >
           {props.reservationType}
@@ -139,6 +139,7 @@ const HistoryElement = (props) => {
       </div>
     </article>
   );
+}
 };
 
 export default HistoryElement;
