5922eec7b881113de54584293f4f20fc5e7dfc5e
김소연 <soyeon.dev@gmail.com>
2022-02-28 23:20:14 +0900

    reservationCancelDetail 컴포넌트 추가

---
+++ src/pages/reservation_history/reservationCancelDetail.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import styles from "./medical_history.module.css";
+import { useLocation } from "react-router";
+import Header from "components/header/header";
+import ReservationStatus from "./component/reservationStatus";
+import MedicalTreatmentInfo from "pages/reservation/components/medicalTreatmentInfo";
+import MySymptom from "./component/mySymptom";
+
+const ReservationCancelDetail = (props) => {
+  const location = useLocation();
+  const reservationInfo = location.state.reservationInfo;
+  const titleTypeDoc = {
+    firstTitle: "유형",
+    secondTitle: "일시",
+  };
+
+  return (
+    <section>
+      {/* <Header title="진료 내역 상세" /> */}
+      <ReservationStatus
+        reservationType="예약취소"
+        reservationInfo={reservationInfo}
+      />
+      <div className={styles.re__reservation__button__area}>
+        <button className={styles.re__reservation__button}>
+          다시 예약하기
+        </button>
+      </div>
+      <div className={styles.seperation__bar}></div>
+    </section>
+  );
+};
+
+export default ReservationCancelDetail;
